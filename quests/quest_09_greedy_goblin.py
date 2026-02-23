8c2586550ab57e6d694e0c0fba448ce569279461
apiel_max <a.piel@alustudent.com>
2026-02-23 19:04:32 +0200

    prompt to calculate the area of rectangle

--- quests/quest_09_greedy_goblin.py
+++
@@ -1,6 +0,0 @@
-gold_pieces = 27
-friends = 4
-each_friend_gets = gold_pieces // friends
-goblin_keeps = gold_pieces % friends
-print(f"Each friend gets {each_friend_gets} gold pieces.")
-print(f"The goblin keeps {goblin_keeps} gold pieces.")
